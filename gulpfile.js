3da63fe85a84e9c932b330345b3525b217d5f356
Leonardo <lrlessa@gmail.com>
2015-10-11 20:32:43 -0300

    improve workflow, not the theme is ready to go

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,56 @@
 var gulp = require('gulp');
 var stylus = require('gulp-stylus');
 var jeet = require('jeet');
-var connect = require('gulp-connect');
-var jade = require('gulp-jade');
+var browserSync = require('browser-sync').create();
+var mustache = require('gulp-mustache');
 var sourcemaps = require('gulp-sourcemaps');
 var consolidate = require('gulp-consolidate');
 var iconfont = require('gulp-iconfont');
-var rename = require("gulp-rename");
+var rename = require('gulp-rename');
+var imagemin = require('gulp-imagemin');
+var pngquant = require('imagemin-pngquant');
+var rupture = require('rupture');
 
+gulp.task('images', function() {
+    return gulp.src('./src/img/*')
+        .pipe(imagemin({
+            progressive: true,
+            optimizationLevel: 7,
+            use: [pngquant()]
+        }))
+        .pipe(gulp.dest('./dist/img/'));
+});
+
+gulp.task('copy', function() {
+    gulp.src('./src/fonts/*')
+        .pipe(gulp.dest('./dist/fonts/'));
+});
 
 gulp.task('css', function() {
     gulp.src('./src/css/styles.styl')
         .pipe(sourcemaps.init())
         .pipe(stylus({
-            use: [jeet()],
-            compress: true
+            use: [jeet(), rupture()],
+            compress: false
         }))
         .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('dist/css'))
-        .pipe(connect.reload());
+        .pipe(browserSync.stream());
 });
 
-gulp.task('connect', function() {
-    connect.server({
-        root: ['./dist'],
-        livereload: true
+gulp.task('browsersync', function() {
+    browserSync.init({
+        server: {
+            baseDir: './dist'
+        }
     });
 });
 
 gulp.task('templates', function() {
-    gulp.src('./src/templates/*.jade')
-        .pipe(jade())
+    gulp.src('./src/templates/*.mustache')
+        .pipe(mustache({}, {extension: '.html'}))
         .pipe(gulp.dest('./dist/'))
-        .pipe(connect.reload());
+        .pipe(browserSync.stream());
 });
 
 gulp.task('iconfont', function(){
@@ -43,18 +61,17 @@ gulp.task('iconfont', function(){
                 .pipe(consolidate('lodash', {
                     glyphs: glyphs,
                     fontName: 'icons',
-                    fontPath: '../fonts/',
-                    className: 's'
+                    className: 'i'
                 }))
                 .pipe(rename({ basename: 'icons' }))
-                .pipe(gulp.dest('src/css/'));
+                .pipe(gulp.dest('src/css/common/'));
         })
         .pipe(gulp.dest('dist/fonts/'));
 });
 
 gulp.task('watch', function() {
     gulp.watch(['src/css/**/*.styl'], ['css']);
-    gulp.watch(['src/templates/*.jade'], ['templates']);
+    gulp.watch(['src/templates/**/*.mustache'], ['templates']);
 });
 
-gulp.task('default', ['css', 'templates' ,'connect', 'watch'])
+gulp.task('default', ['css', 'copy', 'images', 'templates', 'browsersync', 'watch'])
